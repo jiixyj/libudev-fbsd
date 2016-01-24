b2b717786cbec98a8b6f13b4ec0d5dc53dc828fa
Jan Kokemüller <jan.kokemueller@gmail.com>
2016-01-24 15:00:04 +0100

    remove LIBINPUT_EXPORT

--- src/libudev.c
+++ src/libudev.c
@@ -12,9 +12,7 @@
 
 #include <libevdev/libevdev.h>
 
-#define LIBINPUT_EXPORT
 
-LIBINPUT_EXPORT
 struct udev_device *udev_device_new_from_devnum(struct udev *udev, char type,
                                                 dev_t devnum) {
   fprintf(stderr, "udev_device_new_from_devnum %d\n", (int) devnum);
@@ -36,13 +34,11 @@ struct udev_device *udev_device_new_from_devnum(struct udev *udev, char type,
   return NULL;
 }
 
-LIBINPUT_EXPORT
 char const *udev_device_get_devnode(struct udev_device *udev_device) {
   fprintf(stderr, "udev_device_get_devnode\n");
   return udev_device->syspath;
 }
 
-LIBINPUT_EXPORT
 char const *udev_device_get_property_value(struct udev_device *dev,
                                            char const *property) {
   fprintf(stderr, "udev_device_get_property_value %s\n", property);
@@ -110,13 +106,11 @@ char const *udev_device_get_property_value(struct udev_device *dev,
   return retval;
 }
 
-LIBINPUT_EXPORT
 struct udev *udev_device_get_udev(struct udev_device *udev_device) {
   fprintf(stderr, "udev_device_get_udev\n");
   return udev_device->udev;
 }
 
-LIBINPUT_EXPORT
 struct udev_device *udev_device_new_from_syspath(struct udev *udev,
                                                         const char *syspath) {
   fprintf(stderr, "udev_device_new_from_syspath\n");
@@ -132,26 +126,22 @@ struct udev_device *udev_device_new_from_syspath(struct udev *udev,
   return NULL;
 }
 
-LIBINPUT_EXPORT
 const char *udev_device_get_syspath(struct udev_device *udev_device) {
   fprintf(stderr, "udev_device_get_syspath\n");
   return udev_device->syspath;
 }
 
-LIBINPUT_EXPORT
 const char *udev_device_get_sysname(struct udev_device *udev_device) {
   fprintf(stderr, "udev_device_get_sysname\n");
   return udev_device->sysname;
 }
 
-LIBINPUT_EXPORT
 struct udev_device *udev_device_ref(struct udev_device *udev_device) {
   fprintf(stderr, "udev_device_ref\n");
   ++udev_device->refcount;
   return udev_device;
 }
 
-LIBINPUT_EXPORT
 void udev_device_unref(struct udev_device *udev_device) {
   fprintf(stderr, "udev_device_unref %p %d\n", udev_device,
           udev_device->refcount);
@@ -162,28 +152,24 @@ void udev_device_unref(struct udev_device *udev_device) {
   }
 }
 
-LIBINPUT_EXPORT
 struct udev_device *udev_device_get_parent(struct udev_device *udev_device) {
   fprintf(stderr, "udev_device_get_parent %p %d\n", udev_device,
           udev_device->refcount);
   return NULL;
 }
 
-LIBINPUT_EXPORT
 int udev_device_get_is_initialized(struct udev_device *udev_device) {
   fprintf(stderr, "udev_device_get_is_initialized %p %d\n", udev_device,
           udev_device->refcount);
   return 1;
 }
 
-LIBINPUT_EXPORT
 struct udev *udev_ref(struct udev *udev) {
   fprintf(stderr, "udev_ref\n");
   ++udev->refcount;
   return udev;
 }
 
-LIBINPUT_EXPORT
 void udev_unref(struct udev *udev) {
   fprintf(stderr, "udev_unref\n");
   --udev->refcount;
@@ -192,7 +178,6 @@ void udev_unref(struct udev *udev) {
   }
 }
 
-LIBINPUT_EXPORT
 struct udev *udev_new(void) {
   fprintf(stderr, "udev_new\n");
   struct udev *u = calloc(1, sizeof(struct udev));
@@ -203,7 +188,6 @@ struct udev *udev_new(void) {
   return NULL;
 }
 
-LIBINPUT_EXPORT
 struct udev_enumerate *udev_enumerate_new(struct udev *udev) {
   fprintf(stderr, "udev_enumerate_new\n");
   struct udev_enumerate *u = calloc(1, sizeof(struct udev_enumerate));
@@ -214,7 +198,6 @@ struct udev_enumerate *udev_enumerate_new(struct udev *udev) {
   return NULL;
 }
 
-LIBINPUT_EXPORT
 int udev_enumerate_add_match_subsystem(
     struct udev_enumerate *udev_enumerate, const char *subsystem) {
   fprintf(stderr, "udev_enumerate_add_match_subsystem\n");
@@ -234,7 +217,6 @@ static struct udev_list_entry *create_list_entry(char const *path) {
   return le;
 }
 
-LIBINPUT_EXPORT
 void free_dev_list(struct udev_list_entry **list) {
   if (!*list)
     return;
@@ -246,7 +228,6 @@ void free_dev_list(struct udev_list_entry **list) {
   *list = NULL;
 }
 
-LIBINPUT_EXPORT
 int udev_enumerate_scan_devices(struct udev_enumerate *udev_enumerate) {
   fprintf(stderr, "udev_enumerate_scan_devices\n");
 
@@ -280,20 +261,17 @@ int udev_enumerate_scan_devices(struct udev_enumerate *udev_enumerate) {
   return 0;
 }
 
-LIBINPUT_EXPORT
 struct udev_list_entry *udev_enumerate_get_list_entry(
     struct udev_enumerate *udev_enumerate) {
   return udev_enumerate->dev_list;
 }
 
-LIBINPUT_EXPORT
 const char *udev_list_entry_get_name(
     struct udev_list_entry *list_entry) {
   fprintf(stderr, "udev_list_entry_get_name\n");
   return list_entry->path;
 }
 
-LIBINPUT_EXPORT
 void udev_enumerate_unref(struct udev_enumerate *udev_enumerate) {
   fprintf(stderr, "udev_enumerate_unref\n");
   --udev_enumerate->refcount;
@@ -303,7 +281,6 @@ void udev_enumerate_unref(struct udev_enumerate *udev_enumerate) {
   }
 }
 
-LIBINPUT_EXPORT
 struct udev_monitor *udev_monitor_new_from_netlink(struct udev *udev,
                                                           const char *name) {
   fprintf(stderr, "udev_monitor_new_from_netlink %p\n", udev);
@@ -330,7 +307,6 @@ struct udev_monitor *udev_monitor_new_from_netlink(struct udev *udev,
   return u;
 }
 
-LIBINPUT_EXPORT
 int udev_monitor_filter_add_match_subsystem_devtype(
     struct udev_monitor *udev_monitor, const char *subsystem,
     const char *devtype) {
@@ -399,7 +375,6 @@ static void *devd_listener(void *arg) {
   return NULL;
 }
 
-LIBINPUT_EXPORT
 int udev_monitor_enable_receiving(struct udev_monitor *udev_monitor) {
   fprintf(stderr, "udev_monitor_enable_receiving\n");
 
@@ -434,13 +409,11 @@ int udev_monitor_enable_receiving(struct udev_monitor *udev_monitor) {
   }
 }
 
-LIBINPUT_EXPORT
 int udev_monitor_get_fd(struct udev_monitor *udev_monitor) {
   fprintf(stderr, "udev_monitor_get_fd\n");
   return udev_monitor->pipe_fds[0];
 }
 
-LIBINPUT_EXPORT
 struct udev_device *udev_monitor_receive_device(
     struct udev_monitor *udev_monitor) {
   fprintf(stderr, "udev_monitor_receive_device\n");
@@ -469,13 +442,11 @@ struct udev_device *udev_monitor_receive_device(
   return udev_device;
 }
 
-LIBINPUT_EXPORT
 const char *udev_device_get_action(struct udev_device *udev_device) {
   fprintf(stderr, "stub: udev_device_get_action\n");
   return udev_device->action;
 }
 
-LIBINPUT_EXPORT
 void udev_monitor_unref(struct udev_monitor *udev_monitor) {
   fprintf(stderr, "udev_monitor_unref\n");
   --udev_monitor->refcount;
